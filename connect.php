e6a0f18a754161edf4dcaf91d1b7c95bf161ae96
cmann14 <143975495+cmann14@users.noreply.github.com>
2024-04-23 16:50:44 -0400

    Updated

--- connect.php
+++ connect.php
@@ -1,25 +1,55 @@
-<?php
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $email = $_POST['email'];
-    $phoneNumber = $_POST['phoneNumber'];
-    $userName = $_POST['userName'];
-    $password = $_POST['password'];
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Confirmation Page</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel = "stylesheet" href="connect.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display+SC:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">
+</head>
+<body>
+<div class = container>
+    <div class="box form-box">
+        <h2> Residence Radar ◎ </h2>
+        <br>
+        <br>
+        <h3>Welcome to Residence Radar!</h3>
+        <h3>Return to the <a href ="index.html">Homepage</a> to LogIn.</h3>
 
-    //Database connection//
-$conn = new mysqli('localhost', 'root', '', 'connTest');
-if($conn->connect_error){
-    die('Connection Failed : '.$conn->connect_error);
-}else {
-    $stmt = $conn->prepare("insert into registration(firstName, lastName, email, phoneNumber, userName, password)
-        values(?, ?, ?, ?, ?, ?) ");
-    $stmt->bind_param("sssiss", $firstName, $lastName, $email, $phoneNumber, $userName, $password);
-    $stmt->execute();
-    echo "registered successfully";
-    $stmt->close();
-    $conn->close();
 
-}
+        <?php
+            $firstName = $_POST['firstName'];
+            $lastName = $_POST['lastName'];
+            $email = $_POST['email'];
+            $phoneNumber = $_POST['phoneNumber'];
+            $userName = $_POST['userName'];
+            $password = $_POST['password'];
+
+            //Database connection//
+            $conn = new mysqli('localhost', 'root', '', 'connTest');
+            if($conn->connect_error){
+                die('Connection Failed : '.$conn->connect_error);
+            }else {
+                $stmt = $conn->prepare("insert into registration(firstName, lastName, email, phoneNumber, userName, password)
+                values(?, ?, ?, ?, ?, ?) ");
+                $stmt->bind_param("sssiss", $firstName, $lastName, $email, $phoneNumber, $userName, $password);
+                $stmt->execute();
+                $stmt->close();
+                $conn->close();
+            }
+        ?>
+
+
+    </div>
+</div>
+</body>
+</html>
     
 
 
